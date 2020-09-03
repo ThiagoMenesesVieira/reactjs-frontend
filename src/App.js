0163c5b8ddd9023d11bb8de6aa726d0c29a28d61
Thiago Meneses Vieira <thiago.meneses@SPOM010115639.local>
2020-09-02 22:54:10 -0300

    List projects API and create projects

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpeg';
+//import backgroundImage from './assets/background.jpeg';
 
 import Header from './components/Header';
 
 function App() {
 
-    const [ projects, setProjects] = useState(['Desenvolvimento', 'Front-end web']);
+    const [ projects, setProjects] = useState([]);
 
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data)
+        });
+    }, []);
  
 
-    function handleAddProject() {
+    async function handleAddProject() {
         //projects.push(`Novo Projeto ${Date.now()}`)
-        setProjects([...projects,`Novo Projeto ${Date.now()}`])
-        
-        console.log(projects)
+       // setProjects([...projects,`Novo Projeto ${Date.now()}`])
+       const response = await api.post('projects', {
+            "title": `Novo Projeto ${Date.now()}`,
+            "owner": "Thiago Meneses"
+        })
+       
+        const project = response.data;
+
+        setProjects([...projects, project])
 
     }
 
@@ -23,9 +35,9 @@ function App() {
         //fragment: <> </> cria um container para envolver dois ou mais componentes e nao mostrar na arvore de elementos
         <>
             <Header title="HomePage"/>
-            <img width ={100} src={backgroundImage} />
+            
            <ul>
-    {           projects.map(project => <li key={project}>{project}</li>)}
+    {           projects.map(project => <li key={project.id}>{project.title}</li>)}
            </ul>
            <button type="button" onClick={handleAddProject}>Add projects</button>
         </>
